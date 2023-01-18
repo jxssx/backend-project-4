a70b58e7ec12ea027a379de99b9268d5914dfd60
jxssx <danil.druzhina@gmail.com>
2023-01-18 21:14:52 +0300

    fix tests

--- src/utils.js
+++ src/utils.js
@@ -22,20 +22,20 @@ export const buildPathToHtml = (url, output) => {
 };
 
 export const processAssets = (url, data, output) => {
-  const dirName = `${urlToName(url)}_files`
+  const dirName = `${urlToName(url)}_files`;
   const dirPath = path.join(output, dirName);
   const buildAssetLink = (assetLink) => `${assetLink}`;
   const $ = cheerio.load(data);
-  return fsp.mkdir(dirPath).then(() => {
-      $('img').each(function (_, elem) {
-        const attr = elem.attribs.src;
-        const assetLink = buildAssetLink(attr);
-        const assetName = urlToName(assetLink, 'img');
-        axios({
-          url: assetLink,
-          responseType: 'arraybuffer',
-        }).then(({ data }) => fsp.writeFile(path.join(dirPath, assetName), data));
-        $(this).attr("src", path.join(dirName, assetName));
-    })
-  }).then(() => $.html());
+  const assetsPromises = $('img').map(function (_) {
+    const attr = $(this).attr('src');
+    const assetLink = buildAssetLink(attr);
+    const assetName = urlToName(assetLink, 'img');
+    $(this).attr("src", path.join(dirName, assetName));
+    return axios({
+      url: assetLink,
+      responseType: 'arraybuffer',
+    }).then(({ data }) => fsp.writeFile(path.join(dirPath, assetName), data))});
+  return fsp.mkdir(dirPath)
+    .then(() => Promise.all(assetsPromises))
+    .then(() => $.html());
 };
